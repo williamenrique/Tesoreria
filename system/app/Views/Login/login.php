07898a2264c1c35161dfc20d0f491e0983e37e20
williamenrique <william21enrique@gmail.com>
2024-06-26 11:38:32 -0400

    icon

--- system/app/Views/Login/login.php
+++ system/app/Views/Login/login.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="description" content="Responsive Bootstrap 4 Dashboard Template">
-		<meta name="author" content="BootstrapDash">
+		<link rel="icon" type="image/png" href="<?= IMG ?>favicon.ico">
 		<title><?= $data['page_tag'] ?></title>
 		<!-- azia CSS -->
 		<link rel="stylesheet" href="<?= PLUGINS ?>css/sweetalert2.css">
